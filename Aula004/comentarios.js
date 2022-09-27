7c1f485f8d33ccd380a5e2f18bdf2dddd32a8423
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-27 19:57:39 -0300

    Create comentarios.js

---
+++ Aula004/comentarios.js
@@ -0,0 +1,8 @@
+console.log("Hello, World! ")  //Exemplo de comentário em uma linha
+
+/*
+console.log("linha 1 ")
+console.log("linha 2 ")  //Exemplo de comentário em um bloco de linhas (será ignorado pelo Engine do JavaScript)
+console.log("linha 3 ")
+console.log("linha 4 ")
+*/
