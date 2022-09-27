172470af1cbbf84514a61fc18271086d7a1f050e
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-26 22:00:38 -0300

    Create index.js

---
+++ Aula003/Exercicios/index.js
@@ -0,0 +1,3 @@
+console.log(`Meu nome é "Krhyst Ferrari". Estou aprendendo JavaScript as ` + 22 + " da noite. ")  //Exemplo de concatenação
+
+console.log(`Meu nome é "Krhyst Ferrari". Estou aprendendo JavaScript as`, 22,"da noite. ")  //Exemplo de junção (',')
