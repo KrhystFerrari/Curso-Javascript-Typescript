efc8d872254fdd03410d83bf7ec5f5562a37a047
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-26 21:46:58 -0300

    Create index.js

---
+++ Aula003/index.js
@@ -0,0 +1,7 @@
+console.log("Krhyst Ferrari")  //Exemplo do comando console.log
+console.log('Krhyst Ferrari')
+
+var nome = 'Krhyst Ferrari'
+
+console.log(`${nome}`)  //Exemplo de TemplateStrings
+console.log(32, 20000, 30.4000)  //Exemplo de numerais.. todos são do tipo "Number"
