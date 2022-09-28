aaf4f846f7a685ecf8c2f7f86ab2c822a2f8c75e
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-28 20:43:06 -0300

    Update index.js

---
+++ Aula009/index.js
@@ -0,0 +1,3 @@
+nome = 'Krhys';  //NÃO FAÇA ISSO, USE SEMPRE O LET E NUNCA DEIXE VARIÁVEIS SEM DECLARAÇÃO (variáveis declaradas com 'var' podem ser redeclaradas, com 'let' não)
+
+console.log(nome);
