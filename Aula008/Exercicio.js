d6f5ababdd5b6401a1c92aa10b6c309dcf10b304
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-28 20:28:42 -0300

    Create Exercicio.js

---
+++ Aula008/Exercicio.js
@@ -0,0 +1,8 @@
+const nome = 'Krhyst Ferrari';
+const idade = 30;
+const peso = 85;
+const altura = 1.80;
+const imc = peso / altura ** 2;
+const anoNasc = 1992;
+
+console.log(`${nome} tem ${idade} anos, pesa ${peso}Kg, tem ${altura}cm e seu IMC é de ${imc}. ${nome} nasceu em ${anoNasc}. `);  //Exemplo de template strings (placeholder)
