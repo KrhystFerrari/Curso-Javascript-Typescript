513428c32cc802080a3e5b5893e8768f209e7e00
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-27 21:03:45 -0300

    Create index.js

---
+++ Aula006/index.js
@@ -0,0 +1,20 @@
+
+let nome;  //Declaração de variável
+nome = 'João';  //Inicializando a variável
+
+console.log(nome);
+
+nome = 'Maria';  //Exemplo de troca de valor de variável
+
+console.log(nome);
+
+/*
+- Não se pode criar variáveis com palavras reservadas (let, var, if, etc.);
+- Variáveis precisam ter nomes significativos (let nome = 'João';);
+- Não se pode começar o nome de uma variável com um número;
+- Não podem conter espaços ou traços;
+- Utilizar 'camelCase' sempre que possível (let nomeCompleto = 'João Silva';);
+- Case sensitive (letras maiúsculas e minúsculas fazem diferença);
+- Não podemos redeclarar uma variável usando a palavra 'let', 'var';
+- NÃO UTILIZE VAR, UTILIZE LET!
+*/
