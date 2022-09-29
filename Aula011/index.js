f36b30317f38d9a3f8f8dd68e6aa7a4009260209
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-28 21:39:44 -0300

    Create index.js

---
+++ Aula011/index.js
@@ -0,0 +1,32 @@
+/*
+Aritméticos - + - / * ** %
++ - Adição/concatenação;
+- - Subtração;
+/ - Divisão;
+* - Multiplicação;
+** - Potenciação;
+% - Resto da divisão.
+++ - Incremento;
+-- - Decremento;
+atribuição - += -= /= *= %= **=;
+NaN - Not a Number
+
+*/
+
+const num1 = 10;
+const num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+console.log(num1 ** num2);
+console.log(num1 % num2);
+
+/*
+Ordem de precedência:
+1 - () parenteses;
+2- ** potenciação;
+3 -  * / % multiplicação, divisão e resto da divisão (o que vier primeiro lendo da esquerda para a direita);
+4 - + -.
+*/
