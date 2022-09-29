4e767d375a544b4db31b6fbe7b394574d6d8f978
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-28 21:00:33 -0300

    Create index.js

---
+++ Aula010/index.js
@@ -0,0 +1,6 @@
+const nome = 'Krhys';  //String
+const num = 10;  //Number
+const floatNumber = 10.5;  //Number
+let nomeAluno;  //Undefined
+const sobrenomeAluno = null;  //Nulo
+const boolean = true; //Valores lógicos, também pode ser 'false'
